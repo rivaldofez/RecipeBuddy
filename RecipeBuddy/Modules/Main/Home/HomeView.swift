6546190782df8225040d9dc37b0481098509505c
Rivaldo Fernandes <rivaldofez@gmail.com>
2025-08-13 22:19:21 +0700

    Feat: Create base home view

--- RecipeBuddy/Modules/Main/Home/HomeView.swift
+++ RecipeBuddy/Modules/Main/Home/HomeView.swift
@@ -8,12 +8,47 @@
 import SwiftUI
 
 struct HomeView: View {
+    @State var searchQuery: String = ""
+    
+    
     var body: some View {
         ZStack {
-            Text("Home View")
+            VStack(spacing: 0) {
+                HStack {
+                    Text("Recipes")
+                        .font(.plusJakartaBold(size: 18))
+                        .foregroundStyle(Color.clrDarkest)
+                }
+                
+                SearchFieldView(searchText: $searchQuery, placeholderText: "Search recipe")
+                    .clipShape(RoundedRectangle(cornerRadius: 16))
+                    .padding(24)
+                
+                
+                
+                ScrollView {
+                    VStack(spacing: 16) {
+                        Text("My Recipes")
+                            .font(.plusJakartaBold(size: 16))
+                            .foregroundStyle(Color.clrDarkest)
+                            .frame(maxWidth: .infinity, alignment: .leading)
+                            .padding(.horizontal, 24)
+                        
+                        ForEach(0..<20) { item in
+                            CardRecipeView()
+                                .padding(.horizontal, 24)
+                        }
+                    }
+                    .padding(.bottom, 16)
+                }
+
+                
+                Spacer()
+            }
         }
         .frame(maxWidth: .infinity, maxHeight: .infinity)
-        .background(Color.blue)
+        .background(Color.clrPrimaryBackground)
+        .ignoresSafeArea(edges: .bottom)
         
     }
 }
