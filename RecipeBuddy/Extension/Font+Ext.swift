d966ed372059b3aa081798eb0b425d19184b67d8
Rivaldo Fernandes <rivaldofez@gmail.com>
2025-08-13 17:08:42 +0700

    Feat: Create Font extension for add font to view modifier

---
+++ RecipeBuddy/Extension/Font+Ext.swift
@@ -0,0 +1,67 @@
+//
+//  Font+Ext.swift
+//  RecipeBuddy
+//
+//  Created by Rivaldo Fernandes on 13/08/25.
+//
+
+import SwiftUI
+
+extension Font {
+    static func plusJakartaBold(size: CGFloat) -> Font {
+        return Font.custom("PlusJakartaSans-Bold", size: size)
+    }
+    
+    static func plusJakartaBoldItalic(size: CGFloat) -> Font {
+        return Font.custom("PlusJakartaSans-BoldItalic", size: size)
+    }
+    
+    static func plusJakartaExtraBold(size: CGFloat) -> Font {
+        return Font.custom("PlusJakartaSans-ExtraBold", size: size)
+    }
+    
+    static func plusJakartaExtraBoldItalic(size: CGFloat) -> Font {
+        return Font.custom("PlusJakartaSans-ExtraBoldItalic", size: size)
+    }
+    
+    static func plusJakartaExtraLight(size: CGFloat) -> Font {
+        return Font.custom("PlusJakartaSans-ExtraLight", size: size)
+    }
+    
+    static func plusJakartaExtraLightItalic(size: CGFloat) -> Font {
+        return Font.custom("PlusJakartaSans-ExtraLightItalic", size: size)
+    }
+    
+    static func plusJakartaItalic(size: CGFloat) -> Font {
+        return Font.custom("PlusJakartaSans-ExtraLight", size: size)
+    }
+    
+    static func plusJakartaLight(size: CGFloat) -> Font {
+        return Font.custom("PlusJakartaSans-Light", size: size)
+    }
+    
+    static func plusJakartaLightItalic(size: CGFloat) -> Font {
+        return Font.custom("PlusJakartaSans-LightItalic", size: size)
+    }
+    
+    static func plusJakartaMedium(size: CGFloat) -> Font {
+        return Font.custom("PlusJakartaSans-Medium", size: size)
+    }
+    
+    static func plusJakartaMediumItalic(size: CGFloat) -> Font {
+        return Font.custom("PlusJakartaSans-MediumItalic", size: size)
+    }
+    
+    static func plusJakartaRegular(size: CGFloat) -> Font {
+        return Font.custom("PlusJakartaSans-Regular", size: size)
+    }
+    
+    static func plusJakartaSemiBold(size: CGFloat) -> Font {
+        return Font.custom("PlusJakartaSans-SemiBold", size: size)
+    }
+    
+    static func plusJakartaSemiBoldItalic(size: CGFloat) -> Font {
+        return Font.custom("PlusJakartaSans-SemiBoldItalic", size: size)
+    }
+}
+
