bd427a837df53cfa1eead007ee57d65fc511e87f
Rivaldo Fernandes <rivaldofez@gmail.com>
2025-08-13 15:26:20 +0700

    Feat: Initialize project

---
+++ RecipeBuddy/RecipeBuddyApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecipeBuddyApp.swift
+//  RecipeBuddy
+//
+//  Created by Rivaldo Fernandes on 12/08/25.
+//
+
+import SwiftUI
+
+@main
+struct RecipeBuddyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
